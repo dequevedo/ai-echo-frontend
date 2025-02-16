b664741c36fbf7f11a75e9ddfdff923e94c3c9a0
Daniel Quevedo <danielelvisquevedo@gmail.com>
2025-02-16 02:29:41 -0300

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# AI Echo Front-End
+
+### Development server
+
+```ng serve --host 0.0.0.0```
+
+### Developer notes
+
+#### How to create an component
+```
+ng generate component test-button
+```
---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TestButtonComponent } from "./test-button/test-button.component";
+import { TestChatboxComponent } from "./test-chatbox/test-chatbox.component";
+import { AvatarComponent } from "./avatar/avatar.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, TestButtonComponent, TestChatboxComponent, AvatarComponent]
+})
+export class AppComponent {
+  title = 'poc-angular-chat';
+}
